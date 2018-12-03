40ac002b19803abef20449336f262a560bd72a1a
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-04 00:55:21 +0900

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,18 @@
+﻿/**
+* File : 1001.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	printf("%d\n", A - B);
+
+	return 0;
+}
