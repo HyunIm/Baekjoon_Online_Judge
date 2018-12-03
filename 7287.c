b8657e9977876f0ba439fbd0e5451b7a6b54def1
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-04 00:58:55 +0900

    Create 7287.c

---
+++ 7287.c
@@ -0,0 +1,15 @@
+﻿/**
+* File : 7287.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("맞은 문제의 수\n");
+	printf("limhyun\n");
+
+	return 0;
+}
