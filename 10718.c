1e63943311ba4e4daf732bdc9ede57442bf232e0
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-04 01:08:51 +0900

    Create 10718.c

---
+++ 10718.c
@@ -0,0 +1,15 @@
+﻿/**
+* File : 10718.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("강한친구 대한육군\n");
+	printf("강한친구 대한육군\n");
+
+	return 0;
+}
