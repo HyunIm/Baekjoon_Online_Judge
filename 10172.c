19642aeb5fed677ca8ffb5f83874631fc8e3fcb1
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-04 01:03:17 +0900

    Create 10172.c

---
+++ 10172.c
@@ -0,0 +1,18 @@
+﻿/**
+* File : 10172.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+
+	return 0;
+}
