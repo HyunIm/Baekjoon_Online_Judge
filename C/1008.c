901ebaea6aceb717d30e0210d99ad24022a673b5
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-05 01:28:45 +0900

    Create 1008.c

---
+++ C/1008.c
@@ -0,0 +1,18 @@
+﻿/**
+* File : 1008.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 05
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	double A, B;
+
+	scanf("%lf %lf", &A, &B);
+
+	printf("%.9f", A / B);
+
+	return 0;
+}
