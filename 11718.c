bb1b948ae96589f9083866bbd0af9995966ef7d2
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-04 21:57:49 +0900

    Create 11718.c

---
+++ 11718.c
@@ -0,0 +1,23 @@
+﻿/**
+* File : 11718.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	c = getchar();
+
+	while (c != EOF)
+	{
+		putchar(c);
+
+		c = getchar();
+	}
+
+	return 0;
+}
