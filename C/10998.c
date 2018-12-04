3faac1a592bd4aecea32f2b248fca5789e2bdfe6
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-05 01:22:31 +0900

    Create 10998.c

---
+++ C/10998.c
@@ -0,0 +1,18 @@
+﻿/**
+* File : 10998.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 05
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	printf("%d", A * B);
+
+	return 0;
+}
