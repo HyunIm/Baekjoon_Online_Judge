2f2b080d17e53a732528b0937bf8d345254ba541
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-06 01:14:14 +0900

    Create 10869.c

---
+++ C/10869.c
@@ -0,0 +1,22 @@
+﻿/**
+* File : 10869.c
+* Author : 임현 (hyunzion@gmail.com)
+* Since : 2018 - 12 - 06
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int A, B;
+
+	scanf("%d %d", &A, &B);
+
+	printf("%d\n", A + B);
+	printf("%d\n", A - B);
+	printf("%d\n", A * B);
+	printf("%d\n", A / B);
+	printf("%d\n", A % B);
+
+	return 0;
+}
