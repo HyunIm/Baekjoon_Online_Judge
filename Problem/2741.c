17c7e2aee510262252fc26b702631c75ba8467ea
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-23 15:57:11 +0900

    Create 2741.c

---
+++ Problem/2741.c
@@ -0,0 +1,22 @@
+﻿/**
+ * File : 2741.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int n;
+
+	scanf("%d", &n);
+
+	for (i = 1; i < n + 1; i++)
+	{
+		printf("%d\n", i);
+	}
+	
+	return 0;
+}
