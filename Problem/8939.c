261a6a1623dd921c8386e8179a948a48d39f4b16
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-23 20:47:52 +0900

    Create 8939.c

---
+++ Problem/8939.c
@@ -0,0 +1,22 @@
+﻿/**
+ * File : 8939.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, n;
+	int sum = 0;
+
+	scanf("%d", &n);
+
+	for (i = 1; i < n + 1; i++)
+		sum += i;
+
+	printf("%d\n", sum);
+
+	return 0;
+}
