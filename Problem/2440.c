0e1a04a1a1c1c46e9bb92a6f8e54e9b6016c223c
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-23 16:57:49 +0900

    Create 2440.c

---
+++ Problem/2440.c
@@ -0,0 +1,26 @@
+﻿/**
+ * File : 2440.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	int n;
+
+	scanf("%d", &n);
+
+	for (i = n; i > 0; i--)
+	{
+		for (j = i; j > 0; j--)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
