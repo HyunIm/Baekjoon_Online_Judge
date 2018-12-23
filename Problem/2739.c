94aa3cabbc98feaa5c5adf5195c5b3d7a005ad64
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-23 16:15:15 +0900

    Create 2739.c

---
+++ Problem/2739.c
@@ -0,0 +1,22 @@
+﻿/**
+ * File : 2739.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int n;
+
+	scanf("%d", &n);
+
+	for (i = 1; i < 10; i++)
+	{
+		printf("%d * %d = %d\n", n, i, n * i);
+	}
+	
+	return 0;
+}
