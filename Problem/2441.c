6d601f10cb83cb23817bf46bb3aae1c7b0951d9d
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-23 17:01:39 +0900

    Create 2441.c

---
+++ Problem/2441.c
@@ -0,0 +1,32 @@
+﻿/**
+ * File : 2441.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	int n;
+
+	scanf("%d", &n);
+
+	for (i = n; i > 0; i--)
+	{
+		for (j = i; j < n; j++)
+		{
+			printf(" ");
+		}
+
+		for (j = i; j > 0; j--)
+		{
+			printf("*");
+		}
+
+		printf("\n");
+	}
+	
+	return 0;
+}
