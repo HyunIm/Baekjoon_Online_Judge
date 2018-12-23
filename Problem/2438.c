542791e8288ec0217bf1dea1aa36da6f5c43ff37
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-23 16:29:48 +0900

    Create 2438.c

---
+++ Problem/2438.c
@@ -0,0 +1,26 @@
+﻿/**
+ * File : 2438.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i, j;
+	int n;
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i + 1; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
