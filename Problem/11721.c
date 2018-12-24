9a7ca9870c500c77075ceb3917e744a92f38e0d1
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-25 01:08:16 +0900

    Create 11721.c

---
+++ Problem/11721.c
@@ -0,0 +1,27 @@
+﻿/**
+ * File : 11721.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 25
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	char str[100];
+
+	scanf("%s", str);
+
+	while (str[i] != NULL)
+	{
+		printf("%c", str[i]);
+
+		if (i % 10 == 9)
+			printf("\n");
+
+		i++;
+	}
+
+	return 0;
+}
