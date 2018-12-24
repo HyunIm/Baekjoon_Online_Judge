fa4ea45597284bd10ee1af72bcddaf96a72b0d75
Lim Hyun (임현) <hyunzion@gmail.com>
2018-12-25 02:25:52 +0900

    Create 10871.c

---
+++ Problem/10871.c
@@ -0,0 +1,31 @@
+﻿/**
+ * File : 10871.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 25
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int i;
+	int n, x;
+	int *arr;
+
+	scanf("%d %d", &n, &x);
+
+	arr = malloc(sizeof(int) * n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+
+		if (arr[i] < x)
+			printf("%d ", arr[i]);
+	}
+
+	free(arr);
+
+	return 0;
+}
