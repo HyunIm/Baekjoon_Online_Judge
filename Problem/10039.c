97d837a9c09b8157c9cf9aedc979827262290d37
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-04 23:38:11 +0900

    Create 10039.c

---
+++ Problem/10039.c
@@ -0,0 +1,30 @@
+﻿/**
+ * File : 10039.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int sum = 0;
+	int score[5];
+
+	for (i = 0; i < 5; i++)
+	{
+		scanf("%d", &score[i]);
+
+		if (score[i] < 40)
+			score[i] = 40;
+
+		sum += score[i];
+	}
+
+	sum /= 5;
+
+	printf("%d\n", sum);
+	
+	return 0;
+}
