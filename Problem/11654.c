5c4bc7ccf626fb3905a322fc0cf8a97e2d156ace
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-05 00:05:06 +0900

    Create 11654.c

---
+++ Problem/11654.c
@@ -0,0 +1,18 @@
+﻿/**
+ * File : 11654.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 05
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	char c;
+
+	scanf("%c", &c);
+
+	printf("%d\n", c);
+	
+	return 0;
+}
