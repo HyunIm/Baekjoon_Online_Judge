6d272a1cb7ddf7b0d729e0ec6c38512591cf3850
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-04 23:52:20 +0900

    Create 8958.c

---
+++ Problem/8958.c
@@ -0,0 +1,48 @@
+﻿/**
+ * File : 8958.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 04
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+	int i, j;
+	int n;
+	int bonus;
+	int score;
+	int size;
+	char result[80];
+
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++)
+	{
+		scanf("%s", result);
+
+		score = 0;
+		bonus = 1;
+
+		size = strlen(result);
+		for (j = 0; j < size; j++)
+		{
+			if (result[j] == 'O')
+			{
+				score += bonus;
+				bonus++;
+			}
+
+			else
+			{
+				bonus = 1;
+			}
+		}
+
+		printf("%d\n", score);
+	}
+
+	return 0;
+}
