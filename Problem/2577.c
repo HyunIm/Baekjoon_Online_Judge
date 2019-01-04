b81451972b4783e7bc7683a7503b2e620df0ddb9
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-04 21:46:06 +0900

    Create 2577.c

---
+++ Problem/2577.c
@@ -0,0 +1,32 @@
+﻿/**
+ * File : 2577.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int a, b, c;
+	int result;
+	int cnt[10] = { 0, };
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	result = a * b * c;
+
+	while (result)
+	{
+		i = result % 10;
+		result /= 10;
+
+		cnt[i]++;
+	}
+
+	for (i = 0; i < 10; i++)
+		printf("%d\n", cnt[i]);
+
+	return 0;
+}
