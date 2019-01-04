00165c1b03e04d7b53235c8de4f7b7f54bcb5cc4
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-04 23:10:27 +0900

    Create 2920.c

---
+++ Problem/2920.c
@@ -0,0 +1,47 @@
+﻿/**
+ * File : 2920.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 04
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int flag = 0;
+	int sound[8];
+
+	for (i = 0; i < 8; i++)
+		scanf("%d", &sound[i]);
+
+	for (i = 0; i < 8; i++)
+	{
+		if (sound[i] == (i + 1))
+		{
+			flag++;
+			continue;
+		}
+
+		else if (sound[i] == (8 - i))
+		{
+			flag--;
+			continue;
+		}
+
+		else
+		{
+			flag = 0;
+			break;
+		}
+	}
+
+	if (flag == -8)
+		printf("descending\n");
+	else if (flag == 8)
+		printf("ascending\n");
+	else
+		printf("mixed\n");
+	
+	return 0;
+}
