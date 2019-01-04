3f5223820fc08ae267934e4aec052f8a84e69ee1
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-04 23:28:52 +0900

    Create 2742.c

---
+++ Problem/2742.c
@@ -0,0 +1,22 @@
+﻿/**
+ * File : 2742.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2018 - 12 - 23
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+	int n;
+
+	scanf("%d", &n);
+
+	for (i = n; i > 0; i--)
+	{
+		printf("%d\n", i);
+	}
+
+	return 0;
+}
