a94097ec74ce813796f41050e61f12682e4c3d25
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-06 02:52:15 +0900

    Create 2675.c

---
+++ Problem/2675.c
@@ -0,0 +1,37 @@
+﻿/**
+ * File : 2675.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 06
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	int i, j, k;
+	int t;	// 테스트 케이스의 개수
+	int r;	// 반복 횟수
+	int size;	// 문자열의 길이
+	char s[20];	// 문자열
+
+	scanf("%d", &t);
+
+	for (i = 0; i < t; i++)
+	{
+		scanf("%d", &r);
+
+		scanf("%s", s);
+
+		size = strlen(s);
+
+		for (j = 0; j < size; j++)
+		{
+			for (k = 0; k < r; k++)
+				printf("%c", s[j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
+}
