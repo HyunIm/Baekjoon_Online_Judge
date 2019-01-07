f5c8c9d470658db98190365d55d4dd950bba9443
Lim Hyun (임현) <hyunzion@gmail.com>
2019-01-07 22:06:57 +0900

    Create 2292.c

---
+++ Problem/2292.c
@@ -0,0 +1,29 @@
+﻿/**
+ * File : 2292.c
+ * Author : 임현 (hyunzion@gmail.com)
+ * Since : 2019 - 01 - 07
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+	int six = 1;
+	int cnt = 1;
+
+	scanf("%d", &n);
+
+	while (1)
+	{
+		if (n <= six)
+			break;	
+
+		six += 6 * cnt;
+		cnt++;
+	}
+
+	printf("%d\n", cnt);
+
+	return 0;
+}
