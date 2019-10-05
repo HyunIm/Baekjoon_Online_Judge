3c7f59a26896f6205d0050ce8dacd085d50ba02e
Lim Hyun <hyunzion@gmail.com>
2019-10-05 20:49:23 +0900

    Create 10171.c

---
+++ Problem/10171.c
@@ -0,0 +1,17 @@
+/**
+ * File : 10171.c
+ * Author : LimHyun (hyunzion@gmail.com)
+ * Since : 2019 - 10 - 05
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("\\    /\\\n");
+	printf(" )  ( ')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+
+	return 0;
+}
