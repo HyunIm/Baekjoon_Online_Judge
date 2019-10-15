921851a23f2f542dc2cb6e84e29265354219e0ae
Lim Hyun <hyunzion@gmail.com>
2019-10-15 21:38:29 +0900

    Create 10951.c

---
+++ Problem/10951.c
@@ -0,0 +1,19 @@
+/**
+ * File : 10951.c
+ * Author : LimHyun (hyunzion@gmail.com)
+ * Since : 2019 - 10 - 15
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b;
+
+	while (scanf("%d %d", &a, &b) != EOF)
+	{
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
