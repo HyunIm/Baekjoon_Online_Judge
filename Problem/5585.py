1ff9b9bccad158a7c55928ba08373be654817e8a
Lim Hyun <hyunzion@gmail.com>
2021-05-20 20:46:31 +0900

    Update 5585.py

--- Problem/5585.py
+++ Problem/5585.py
@@ -4,26 +4,11 @@
 # Brief : 그리디 알고리즘
 
 n = 1000 - (int(input()))
+coin_type = [500, 100, 50, 10, 5, 1]
 cnt = 0
 
-while n > 0:
-    if n >= 500:
-        n -= 500
-        cnt += 1
-    elif n >= 100:
-        n -= 100
-        cnt += 1
-    elif n >= 50:
-        n -= 50
-        cnt += 1
-    elif n >= 10:
-        n -= 10
-        cnt += 1
-    elif n >= 5:
-        n -= 5
-        cnt += 1
-    else :
-        n -= 1
-        cnt += 1
+for coin in coin_type:
+    cnt += n // coin
+    n %= coin
 
 print(cnt)
