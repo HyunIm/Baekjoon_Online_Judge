c122570f661aa68ace5f9766ed517e258d3352b2
Lim Hyun <hyunzion@gmail.com>
2021-07-18 17:33:45 +0900

    Update 1439.py

--- Problem/1439.py
+++ Problem/1439.py
@@ -1,22 +1,16 @@
-# File : 1439.py
+# File : 11-3.py
 # Dev : LimHyun (hyunzion@gmail.com)
-# Since : 2021-05-23
-# Brief : 그리디 알고리즘
+# Since : 2021-07-18
+# Brief : 그리디
 
 s = input()
-zero = 0
-one = 0
-
-if (s[0] == '0'):
-    one += 1
-else:
-    zero += 1
-
-for i in range(len(s) - 1):
-    if s[i] != s[i + 1]:
-        if s[i + 1] == '0':
-            one += 1
-        else:
-            zero += 1
-
-print(min(zero, one))
+f = s[0]
+a = int(s[0])
+b = 0 if int(s[0]) else 1
+for i in range(1, len(s)):
+    if f=='0' and s[i]=='1':
+        a += 1
+    elif f=='1' and s[i]=='0':
+        b += 1
+    f = s[i]
+print(min(a, b))
