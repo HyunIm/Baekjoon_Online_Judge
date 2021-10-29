28a155cbf3c3fd6eed2bfb3a938c36e7662c17f0
Lim Hyun <hyunzion@gmail.com>
2021-10-29 21:25:44 +0900

    Add Implement Problem

---
+++ Problem/5339.py
@@ -0,0 +1,6 @@
+# File : 5339.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-10-29
+# Brief : 구현
+
+print('     /~\\\n    ( oo|\n    _\=/_\n   /  _  \\\n  //|/.\|\\\\\n ||  \ /  ||\n============\n|          |\n|          |\n|          |')
