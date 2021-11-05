1c71dacbe7d2714895e861f2de5114cbda8150e4
Lim Hyun <hyunzion@gmail.com>
2021-11-05 21:17:08 +0900

    Add Implement Problem

---
+++ Problem/10170.py
@@ -0,0 +1,26 @@
+# File : 10170.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-11-05
+# Brief : 구현
+
+def printNfcWest():
+    print('NFC West       W   L  T')
+    print('-----------------------')
+    print('Seattle        13  3  0')
+    print('San Francisco  12  4  0')
+    print('Arizona        10  6  0')
+    print('St. Louis      7   9  0')
+
+
+def printNfcNorth():
+    print('NFC North      W   L  T')
+    print('-----------------------')
+    print('Green Bay      8   7  1')
+    print('Chicago        8   8  0')
+    print('Detroit        7   9  0')
+    print('Minnesota      5  10  1')
+
+
+printNfcWest()
+print()
+printNfcNorth()
