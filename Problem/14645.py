a9c37c0915bfd02fd8c805dfda467320ae203c18
Lim Hyun <hyunzion@gmail.com>
2021-11-15 20:57:23 +0900

    Add Implement Problem

---
+++ Problem/14645.py
@@ -0,0 +1,8 @@
+# File : 14645.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-11-15
+# Brief : 구현
+
+n, k = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(n)]
+print('비와이')
