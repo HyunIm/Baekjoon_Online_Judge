a45238482affce518e49bdeb959e4bf409a166a2
Lim Hyun <hyunzion@gmail.com>
2021-11-27 17:43:52 +0900

    Add Implement Problem

---
+++ Problem/16170.py
@@ -0,0 +1,8 @@
+# File : 16170.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-11-27
+# Brief : 구현
+
+import datetime
+d = str(datetime.datetime.now())
+print(d[:4], d[5:7], d[8:10], sep='\n')
