7b3c4cb19b9c8b4e71931622dcff57c8137d52a3
Lim Hyun <hyunzion@gmail.com>
2021-11-30 21:18:14 +0900

    Add Math Problem

---
+++ Problem/17256.py
@@ -0,0 +1,8 @@
+# File : 17256.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-11-30
+# Brief : 수학
+
+ax, ay, az = map(int, input().split())
+cx, cy, cz = map(int, input().split())
+print(cx-az, cy//ay, cz-ax)
