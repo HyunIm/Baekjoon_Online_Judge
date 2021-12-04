6f30e05277eb401fd70073dd23fba99eafebbf58
Lim Hyun <hyunzion@gmail.com>
2021-12-04 14:28:10 +0900

    Add Math Problem

---
+++ Problem/18096.py
@@ -0,0 +1,6 @@
+# File : 18096.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-12-04
+# Brief : 수학
+
+print(1)
---
+++ Problem/18108.py
@@ -0,0 +1,8 @@
+# File : 18108.py
+# Dev : LimHyun (hyunzion@gmail.com)
+# Since : 2021-12-04
+# Brief : 수학
+
+y = int(input())
+buddhistCalendar = y - 543
+print(buddhistCalendar)
