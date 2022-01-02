a096d5b571c0003d54accda74c758179d4e7a9f5
Lim Hyun <hyunzion@gmail.com>
2022-01-02 14:00:50 +0900

    Add Implement Problem

---
+++ Problem/10768.py
@@ -0,0 +1,14 @@
+def main():
+    month = int(input())
+    day = int(input())
+
+    if month == 2 and day == 18:
+        print('Special')
+    elif month == 1 or (month == 2 and day < 18):
+        print('Before')
+    else:
+        print('After')
+
+
+if __name__ == '__main__':
+    main()
