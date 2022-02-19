0899fb51de86a6019fa22b33b5f27bf7951dbe46
Lim Hyun <hyunzion@gmail.com>
2022-02-19 10:27:40 +0000

    Update Greedy Problem

--- Problem/1439.py
+++ Problem/1439.py
@@ -1,16 +1,16 @@
-# File : 11-3.py
-# Dev : LimHyun (hyunzion@gmail.com)
-# Since : 2021-07-18
-# Brief : 그리디
+def flipCounter(S, flip):
+    count = 0
+    
+    for i in range(len(S)):
+        if S[i] == flip:
+            if i == 0 or S[i-1] != S[i]:
+                count += 1
 
-s = input()
-f = s[0]
-a = int(s[0])
-b = 0 if int(s[0]) else 1
-for i in range(1, len(s)):
-    if f=='0' and s[i]=='1':
-        a += 1
-    elif f=='1' and s[i]=='0':
-        b += 1
-    f = s[i]
-print(min(a, b))
+    return count
+
+
+S = input()
+zeroFlip = flipCounter(S, '0')
+oneFlip = flipCounter(S, '1')
+
+print(min(zeroFlip, oneFlip))
